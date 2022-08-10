d68530e6b5aadbc26ee54a45b767f310dbfbec87
YuryAntilevsky <ura1693@mail.ru>
2022-08-10 20:32:45 +0300

    feat add html css

---
+++ js_exemple/scrypt.js
@@ -0,0 +1,14 @@
+'use strict'
+//number  1, 2, +infinity, -infinity, 0.3, NaN 
+//string  'dog', "cat", `cow`
+//boolean  true, falce
+//object  {}, [], null
+//undefined  undefined
+//null  
+//bigint 1n
+//symbol 
+// +, -, /, *, **, %, =, ==, ===, 
+// var, let, const
+ let a = 1
+ let a = 2
+console.log(a)
